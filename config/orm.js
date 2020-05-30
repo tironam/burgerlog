9ba790b47acc995519b94f9da179018ffe1fdaa6
Michael Tirona <tironamichael@gmail.com>
2020-05-30 13:12:17 -0700

    Added code to models

--- config/orm.js
+++ config/orm.js
@@ -7,16 +7,15 @@ module.exports = {
             cb(data)
         })
     },
-    getAllWhere (table, where, cb) {
-        db.query(`SELECT * FROM ${table} WHERE ?`, where, (err, data) => {
-            if (err) { console.log(err) }
-            cb(data)
-        })
-    },
     createOne (table, data, cb) {
         db.query(`INSERT INTO ${table} SET ?`, data, (err, info) => {
             if (err) { console.log(err) }
             cb(data)
         })
     },
-    
+    updateOne (table, changes, where, cb) {
+        db.query(`UPDATE ${table} SET ? WHERE ?`, [changes, where, (err, info) +> {
+            if (err) { console.,log(err) }
+            cb(info)
+        }])
+    }
